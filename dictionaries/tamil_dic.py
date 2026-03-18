1694f638821a72582a19a7e05a5451920a16031f
SPARKTXTFF <sparkstreamlabs@gmail.com>
2026-03-18 23:09:06 +0530

    Update tamil_dic.py

--- dictionaries/tamil_dic.py
+++ dictionaries/tamil_dic.py
@@ -1,42 +1,26 @@
 dictionary = {
-            'எனில்': 'if', 'if': 'if',
-            'ஆனால்': 'elif', 'elif': 'elif',
-            'இல்லை': 'else', 'else': 'else',
-
-            # loops
-            'சுற்று': 'for', 'for': 'for',
-            'வரை': 'while', 'while': 'while',
-            'இல்': 'in', 'in': 'in',
-
-            # flow
-            'நிறுத்து': 'break', 'stop': 'break',
-            'தொடர்': 'continue', 'thodar': 'continue', 'continue' : 'continue',
-
-            # logic
-            'மற்றும்': 'and', 'matrum': 'and', 'and': 'and',
-            'அல்லது': 'or', 'allathu': 'or', 'or': 'or',
-            'இல்லாத': 'not', 'illatha': 'not', 'not': 'not',
-
-            # values
-            'உண்மை': 'True', 'true': 'True',
-            'பொய்': 'False', 'false': 'False',
-            'ஏதுமில்லை': 'None', 'none': 'None',
-
-            # functions
-            'செயல்': 'def', 'seyal': 'def', 'function': 'def', 'def': 'def',
-            'திருப்பு': 'return', 'thiruppu': 'return', 'return': 'return',
-
-            # print
-            'பதி': 'print', 'padi': 'print', 'print': 'print',
-            'காட்டு': 'print', 'kaattu': 'print',
-            'சொல்லு': 'print', 'sollu': 'print',
-
-            # input
-            'உள்ளிடு': 'input', 'ullidu': 'input', 'input': 'input',
-
-            # misc
-            'நீளம்': 'len', 'len': 'len',
-            'முழுஎண்': 'int', 'int': 'int',
-            'சரம்': 'str', 'str': 'str',
-            'பட்டியல்': 'list', 'list': 'list'
+    'எனில்': 'if', 'if': 'if',
+    'ஆனால்': 'elif', 'elif': 'elif',
+    'இல்லை': 'else', 'else': 'else',
+    'சுற்று': 'for', 'for': 'for',
+    'வரை': 'while', 'while': 'while',
+    'இல்': 'in', 'in': 'in',
+    'நிறுத்து': 'break', 'stop': 'break',
+    'தொடர்': 'continue', 'thodar': 'continue', 'continue' : 'continue',
+    'மற்றும்': 'and', 'matrum': 'and', 'and': 'and',
+    'அல்லது': 'or', 'allathu': 'or', 'or': 'or',
+    'இல்லாத': 'not', 'illatha': 'not', 'not': 'not',
+    'உண்மை': 'True', 'true': 'True',
+    'பொய்': 'False', 'false': 'False',
+    'ஏதுமில்லை': 'None', 'none': 'None',
+    'செயல்': 'def', 'seyal': 'def', 'function': 'def', 'def': 'def',
+    'திருப்பு': 'return', 'thiruppu': 'return', 'return': 'return',
+    'பதி': 'print', 'padi': 'print', 'print': 'print',
+    'காட்டு': 'print', 'kaattu': 'print',
+    'சொல்லு': 'print', 'sollu': 'print',
+    'உள்ளிடு': 'input', 'ullidu': 'input', 'input': 'input',
+    'நீளம்': 'len', 'len': 'len',
+    'முழுஎண்': 'int', 'int': 'int',
+    'சரம்': 'str', 'str': 'str',
+    'பட்டியல்': 'list', 'list': 'list'
 }
